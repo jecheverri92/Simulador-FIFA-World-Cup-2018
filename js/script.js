3df592f5766af49a9079409d204ca9814b3e500c
Julian <32905680+jecheverri92@users.noreply.github.com>
2018-06-18 22:25:26 -0500

    Se agrego html grupos con JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,70 @@
 
 // MUNDIAL CONTROLLER
 
-
 var worldCupController = (function(){   
-    var getDataGroups = function() {
+
+    var datos;
+
+    return {
+        getDataGroups: function() {
         fetch("https://raw.githubusercontent.com/openfootball/world-cup.json/master/2018/worldcup.groups.json")
-        .then(res => res.json()).then(data => {
-            console.log(data.groups);
-             })
+        .then(res => res.json()).then(res => {
+            console.log(res.groups);
+            res.groups.forEach(g => {
+                
+                UIController.addListItem(g);
+            })
+            
+         })
+    
+        },
+
+         displayData: function() {
+             console.log(datos);
+
+         }
     }
+
+   
+
+
 })(); 
 
 
 // UI CONTROLLER
 var UIController = (function() {
+    var DOMStrings = {
+        gruopLabel: ".group__title",
+        teamLabel: ".group__title", 
+        divGroups: "#grupos" 
+    }
+
+    return {
+        addListItem: function(obj) {
+            var html, newHtml, element;
+
+            element = document.querySelector(DOMStrings.divGroups);
+            html =`<div class ="group">
+            <details><summary class ="group__title">${obj.name}</summary>
+            <ul class ="group__teams">
+            
+                <li class="group__team"><i class="flag-icon flag-icon-ru "></i>${obj.teams[0].name}</li>
+                <li class="group__team"><i class="flag-icon flag-icon-ru "></i>${obj.teams[1].name}</li>
+                <li class="group__team"><i class="flag-icon flag-icon-ru "></i>${obj.teams[2].name}</li>
+                <li class="group__team"><i class="flag-icon flag-icon-ru "></i>${obj.teams[3].name}</li>
+            
+            </ul></details>
+            </div>`
+           
+            // Replace the placeholder text with some actual data
+            element.insertAdjacentHTML("beforeEnd", html);
+        
+        }
+    
 
 
+
+    }
 })(); 
 
 
@@ -23,10 +72,24 @@ var UIController = (function() {
 
 var controller = (function(wCupCtrl, UICtrl) {
 
-    var ctrlAddItem = function() {
 
-        // 1. Obtener JSON
+    var  data;
 
-        // 2. Add item to the UI
+    var addItems = function(datos){
+        UICtrl.addListItem()
+
+    }
+
+    return {
+        init: function() {
+            // 1. Obtener JSON
+            wCupCtrl.getDataGroups();
+
+            console.log(data);
+            // 2. Update UI
+
+        }
     }
 })(worldCupController, UIController);
+
+controller.init();
